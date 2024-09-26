7722a3e50a2efbd9ac85259477bb10e222600bd8
miiiingi <kimmingi722@gmail.com>
2024-09-26 22:53:36 +0900

    base chat prgram / rtsp + message send, receive

--- ChatProgram-Server.pro
+++ ChatProgram-Server.pro
@@ -1,12 +1,13 @@
 QT       += core gui
 QT       += network
-QT += multimedia multimediawidgets
+QT       += multimedia multimediawidgets
+QT       += sql
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 CONFIG += c++17
 
 # GStreamer를 pkg-config로 설정
-PKGCONFIG += gstreamer-1.0 gstreamer-video-1.0
+PKGCONFIG += gstreamer-1.0 gstreamer-video-1.0 gstreamer-rtsp-server-1.0
 
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
@@ -17,7 +18,6 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
-    gst.h \
     mainwindow.h
 
 FORMS += \
@@ -27,6 +27,25 @@ TRANSLATIONS += \
     ChatProgram-Server_ko_KR.ts
 CONFIG += lrelease
 CONFIG += embed_translations
+
+# Include paths for GStreamer and GLib
+INCLUDEPATH += /opt/homebrew/Cellar/glib/2.82.1/include/glib-2.0 \
+               /opt/homebrew/Cellar/glib/2.82.1/lib/glib-2.0/include \
+               /opt/homebrew/Cellar/gstreamer/1.24.8/include/gstreamer-1.0 \
+               /opt/homebrew/Cellar/gstreamer/1.24.8/include/gst
+
+LIBS += -L/opt/homebrew/Cellar/glib/2.82.1/lib \
+        -L/opt/homebrew/Cellar/gstreamer/1.24.8/lib \
+        -lgstreamer-1.0 \
+        -lgstapp-1.0 \
+        -lgstbase-1.0 \
+        -lgstvideo-1.0 \
+        -lgstpbutils-1.0 \
+        -lgstplay-1.0 \
+        -lgstnet-1.0 \
+        -lgstrtspserver-1.0 \
+        -lglib-2.0 \
+        -lgobject-2.0
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
--- main.cpp
+++ main.cpp
@@ -1,12 +1,78 @@
+#include <gst/gst.h>
+#include <gst/rtsp-server/rtsp-server.h>
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
-
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QCoreApplication>
 #include <QSet>
+#include <QFileInfo>
+#include <QFile>
+#include <QProcess>
+#include <iostream>
+#include <QThread>
 
+// RTSP 서버를 별도의 스레드에서 실행하는 클래스
+class RTSPServerThread : public QThread {
+    Q_OBJECT
+
+public:
+    RTSPServerThread(QObject *parent = nullptr) : QThread(parent) {}
+
+    void run() override {
+        GMainLoop *loop = g_main_loop_new(nullptr, FALSE);
+        if (!loop) {
+            qCritical() << "GMainLoop를 생성할 수 없습니다.";
+            return;
+        }
+        qInfo() << "GMainLoop 생성됨.";
+
+        // GstRTSPServer 생성
+        GstRTSPServer *server = gst_rtsp_server_new();
+        if (!server) {
+            qCritical() << "RTSP 서버를 생성할 수 없습니다.";
+            return;
+        }
+        qInfo() << "RTSP 서버 생성됨.";
+
+        // Mount points 설정
+        GstRTSPMountPoints *mounts = gst_rtsp_server_get_mount_points(server);
+        if (!mounts) {
+            qCritical() << "Mount points를 얻을 수 없습니다.";
+            return;
+        }
+        qInfo() << "Mount points 설정됨.";
+
+        // 미디어 팩토리 생성 및 파이프라인 설정
+        GstRTSPMediaFactory *factory = gst_rtsp_media_factory_new();
+        if (!factory) {
+            qCritical() << "미디어 팩토리를 생성할 수 없습니다.";
+            g_object_unref(mounts);
+            return;
+        }
+        qInfo() << "미디어 팩토리 생성됨.";
+        const char *pipeline_desc = "( videotestsrc ! videoconvert ! x264enc ! rtph264pay name=pay0 pt=96 )";
+
+        gst_rtsp_media_factory_set_launch(factory, pipeline_desc);
+        gst_rtsp_mount_points_add_factory(mounts, "/test", factory);
+        qInfo() << "/test 경로에 미디어 팩토리 마운트됨.";
+
+        g_object_unref(mounts);
+
+        guint id = gst_rtsp_server_attach(server, nullptr);
+        if (id == 0) {
+            qCritical() << "RTSP 서버를 네트워크에 연결할 수 없습니다.";
+            return;
+        }
+        qInfo() << "RTSP 서버가 rtsp://127.0.0.1:8554/test 에서 실행 중입니다.";
+
+        // GMainLoop 실행 (별도의 스레드에서)
+        g_main_loop_run(loop);
+    }
+};
+
+// QT 서버 클래스
 class ChatServer : public QTcpServer {
     Q_OBJECT
 
@@ -16,16 +82,36 @@ public:
 protected:
     void incomingConnection(qintptr socketDescriptor) override {
         QTcpSocket *clientSocket = new QTcpSocket(this);
-        clientSocket->setSocketDescriptor(socketDescriptor);
+        if (!clientSocket->setSocketDescriptor(socketDescriptor)) {
+            delete clientSocket;
+            return;
+        }
         clients.insert(clientSocket);
 
         // 클라이언트로부터 메시지를 받았을 때
         connect(clientSocket, &QTcpSocket::readyRead, [this, clientSocket]() {
             QByteArray message = clientSocket->readAll();
-            broadcastMessage(message, clientSocket);
+            QString messageStr = QString::fromUtf8(message);
+
+            QStringList messageList = messageStr.split("\n", Qt::SkipEmptyParts);  // '\n'으로 메시지 구분
+            for (const QString& msg : messageList) {
+                qInfo() << "Received message from client: " << msg;
+
+                if (msg == "WEBCAM_STREAM") {
+                    startRTSPServer();
+                    qInfo() << "RTSP 서버 실행 및 웹캠 스트림 시작";
+                } else if (msg == "PLAY") {
+                    qInfo() << "Play button clicked";
+                    handlePlayRequest(clientSocket);
+                } else {
+                    broadcastMessageToClients(msg, clientSocket);  // 다른 클라이언트에게 메시지 전송
+                }
+
+            }
         });
 
-        // 클라이언트 연결이 끊어졌을 때
+
+        // 클라이언트 연결 해제 시
         connect(clientSocket, &QTcpSocket::disconnected, [this, clientSocket]() {
             clients.remove(clientSocket);
             clientSocket->deleteLater();
@@ -33,28 +119,42 @@ protected:
     }
 
 private:
-    QSet<QTcpSocket *> clients;
+    QSet<QTcpSocket *> clients;    // 클라이언트에 RTSP URL 전송
+    // RTSP 서버를 별도의 스레드에서 실행
+    void startRTSPServer() {
+        RTSPServerThread *rtspThread = new RTSPServerThread(this);
+        rtspThread->start();  // 별도의 스레드에서 GMainLoop 실행
+    }
 
-    // 메시지 브로드캐스트 함수
-    void broadcastMessage(const QByteArray &message, QTcpSocket *sender) {
+    void handlePlayRequest(QTcpSocket *clientSocket) {
+        QString rtspUrl = "rtsp://127.0.0.1:8554/test";
+        clientSocket->write(rtspUrl.toUtf8());  // 클라이언트에 RTSP URL 전송
+        qInfo() << "클라이언트에게 RTSP 스트림 URL 전송: " << rtspUrl;
+    }
+    // 다른 클라이언트에게 메시지를 브로드캐스트하는 함수
+    void broadcastMessageToClients(const QString &message, QTcpSocket *sender) {
         for (QTcpSocket *client : clients) {
-            if (client != sender) { // 메시지를 보낸 클라이언트는 제외
-                client->write(message);
+            if (client != sender) {  // 메시지를 보낸 클라이언트는 제외
+                client->write(message.toUtf8());
             }
         }
+        qInfo() << "Broadcasted message to other clients: " << message;
     }
 };
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
+    // GStreamer 초기화
+    gst_init(&argc, &argv);
+
     ChatServer server;
     if (!server.listen(QHostAddress::Any, 1234)) {
-        qCritical() << "Unable to start server";
+        qCritical() << "서버를 시작할 수 없습니다.";
         return 1;
     }
 
-    qInfo() << "Chat server started on port 1234";
+    qInfo() << "Chat 서버가 포트 1234에서 시작되었습니다.";
     return app.exec();
 }
 
